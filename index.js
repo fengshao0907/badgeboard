b0f87e465f82ca562d7ec36dc9f4a80b0cda141a
xudafeng <xudafeng@126.com>
2015-11-11 17:26:37 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* ================================================================
+ * badgeboard by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Nov 11 2015 17:09:45 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+module.exports = require('./lib/badgeboard');
---
+++ test/badgeboard.test.js
@@ -0,0 +1,19 @@
+/* ================================================================
+ * badgeboard by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Nov 11 2015 17:09:45 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var badgeboard = require('../');
+
+describe('test', function(){
+});
